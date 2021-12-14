793a2585d9fb914c591fbc5748dc9eba37fb620c
hkhan11 <76545669+hkhan11@users.noreply.github.com>
2021-12-14 12:36:38 -0500

    Adding assigment file

---
+++ Assigment.ps1
@@ -0,0 +1,36 @@
+﻿function find-recurringchar
+{
+	
+	[CmdletBinding()]
+	param (
+		[char[]]$foo
+		
+	)
+	
+	$foo2 = [System.Collections.Generic.HashSet[char]]::new($foo);
+	[array]$dupChar = foreach ($i in $foo2)
+	{
+		$global:cntFound = 1
+		foreach ($f in $foo)
+		{
+            
+			if ($i -eq $f)
+			{
+				$global:cntFound++;
+			};
+		};
+		
+		if ($global:cntFound -ge 3)
+		{
+			$i
+            
+		};
+	};
+	
+	$outstring = "List first recurring character found: `n`r $($dupChar | Select-Object -first 1 $_) `n`r";
+	
+	
+	Write-Host "$outstring" ;
+	
+};
+
